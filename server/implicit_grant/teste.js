ab441dde626b1bf0a0845fb1dc1235feaa99a7b5
Pedro Afonso <pedroafonso@air-de-pedro.home>
2014-07-31 23:48:38 +0100

    hangthedj

--- server/implicit_grant/teste.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8888, '192.168.1.5');
-console.log('Server running at http://192.168.1.5:8888/');
